c449b71883e55a6ee617fe4a2d76b57fc309402b
vantiya <parvezvantiya@gmail.com>
2020-12-28 23:05:39 +0530

    Added GoogleLogin + Firebase Part-1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,12 @@
+// import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyFirstApp());
 }
 
@@ -14,7 +20,7 @@ class MyFirstApp extends StatelessWidget {
       ),
       home: MyHomePage(
         title: "Welcome to My First Practice App",
-        bodyText: "This is My First Practice App's Body Text :)",
+        bodyText: "I'm now trying Firebase.",
       ),
     );
   }
@@ -29,15 +35,56 @@ class MyHomePage extends StatefulWidget {
 }
 
 class MyClassWidgetState extends State<MyHomePage> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  final GoogleSignIn googleSignIn = GoogleSignIn();
+
+  Future<String> signInWithGoogle() async {
+    final GoogleSignInAccount googleUser = await googleSignIn.signIn();
+    final GoogleSignInAuthentication googleAuth =
+        await googleUser.authentication;
+    final AuthCredential _authCred = GoogleAuthProvider.credential(
+        idToken: googleAuth.idToken, accessToken: googleAuth.accessToken);
+
+    final UserCredential authResult =
+        await _auth.signInWithCredential(_authCred);
+
+    final User user = authResult.user;
+    if (user != null) {
+      print("User Details: $user");
+      return "$user";
+    } else {
+      print("Something Wrong!");
+      return "";
+    }
+  }
+
+  Future<void> signOutGoogle() async {
+    await googleSignIn.signOut();
+    print("User Signed Out!");
+    // return "";
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Text(widget.bodyText),
-      ),
-    );
+        appBar: AppBar(
+          title: Text(widget.title),
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            RaisedButton(
+              child: Text("Login"),
+              color: Colors.lightBlue,
+              onPressed: signInWithGoogle,
+            ),
+            RaisedButton(
+              child: Text("Logout"),
+              color: Colors.lightBlue,
+              onPressed: signOutGoogle,
+            )
+          ],
+        ));
   }
 }
